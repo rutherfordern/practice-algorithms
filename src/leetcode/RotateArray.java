0c28242946508d47d1dacda81f66c5d3f510353c
Egor <glavnegor@yandex.ru>
2022-03-17 15:13:29 +0500

    add task

---
+++ src/leetcode/RotateArray.java
@@ -0,0 +1,41 @@
+package leetcode;
+
+import java.util.Arrays;
+
+public class RotateArray {
+    public static void main(String[] args) {
+
+        int[] nums = {1, 2, 3, 4, 5, 6, 7};
+
+        rotate1(nums, 3);
+
+        System.out.println(Arrays.toString(nums));
+
+    }
+
+    public static void rotate1(int[] nums, int k) {
+
+        for (int i = 0; i < k; i++) {
+            for (int j = nums.length - 1; j > 0; j--) {
+                int temp = nums[j];
+                nums[j] = nums[j - 1];
+                nums[j - 1] = temp;
+            }
+        }
+    }
+
+    public static void rotate2(int[] nums, int k) {
+
+        int[] newSums = new int[nums.length];
+
+        for (int i = 0; i < nums.length; i++) {
+            int j = (i + k) % nums.length;
+            newSums[j] = nums[i];
+        }
+
+        for (int i = 0; i < nums.length; i ++) {
+            nums[i] = newSums[i];
+        }
+
+    }
+}
---
+++ src/leetcode/SquaresOfaSortedArray.java
@@ -0,0 +1,27 @@
+package leetcode;
+
+import java.util.Arrays;
+
+public class SquaresOfaSortedArray {
+    public static void main(String[] args) {
+
+        int[] nums = {-4, -1, 0, 3, 10};
+
+        System.out.println(Arrays.toString(sortedSquares(nums)));
+
+        //Arrays.toString(sortedSquares(nums));
+
+    }
+
+    public static int[] sortedSquares(int[] nums) {
+
+        for (int i = 0; i < nums.length; i++) {
+            nums[i] = nums[i] * nums[i];
+        }
+
+        Arrays.sort(nums);
+
+        return nums;
+
+    }
+}
